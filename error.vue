be0572577f22d1ac2955594a83d7f279e87e0711
AndersNielsen85 <anders.b.nielsen85@gmail.com>
2022-08-16 09:29:21 +0200

    2 files added, 1 deleted

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    This will be shown on errors<br>
+     Code: {{ error.statusCode }}<br>
+     Message: {{ error.statusMessage }}
+  </div>
+  <button @click="handleError">Clear error</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
