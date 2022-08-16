ec6dbb91d009c17ce56daad39fa9ba03396f1724
AndersNielsen85 <anders.b.nielsen85@gmail.com>
2022-08-16 12:27:23 +0200

    1 file modified

--- pages/test404.vue
+++ pages/test404.vue
@@ -3,18 +3,11 @@
     {{ data }}
   </div>
 </template>
-<script lang="ts">
-
-export default defineComponent({
-  async setup() {
-    await Promise.resolve(true);
+<script setup lang="ts">
 const route = useRoute();
 const { data, refresh } = await useFetch('https://httpstat.us/404');
 
 if (!data.value) {
-  console.log('throw error')
   throw createError({ statusCode: 404, statusMessage: 'Something went wrong' })
 }
-  },
-});
 </script>
